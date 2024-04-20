971b385ed54b80f96f7e1ee25340925243d14b4a
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-04-20 17:48:38 +0530

    hero page done

--- src/components/hero/HeroPage.js
+++ src/components/hero/HeroPage.js
@@ -1,9 +1,9 @@
 import './HeroPage.scss';
 import heroImg from '../../images/hero.svg';
 import { Button, Container, Row, Col } from 'react-bootstrap';
-import { motion, animate } from 'framer-motion';
+import { motion } from 'framer-motion';
 
-const textVarients = {
+const textVarient = {
     initial: {
         x: -500,
         opacity: 0,
@@ -18,15 +18,31 @@ const textVarients = {
     }
 }
 
+const imgVarient = {
+    initial: {
+        x: 500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+
+};
+
 const sliderVariant = {
     initial: {
         x: 0,
     },
     animate: {
-        x: "220%",
+        x: "-220%",
         transition: {
             repeat: Infinity,
-            duration: 20,
+            duration: 50,
         }
     }
 };
@@ -37,29 +53,33 @@ const HeroPage = () => {
             <Container className='d-flex justify-content-center align-items-center mt-5'>
                 <Row>
                     <Col lg={6}>
-                        <div className="textCont d-flex flex-column justify-content-center" >
-                            <h2>NEEHARIKA S</h2>
-                            <h1>Cyber Security Analyst</h1>
-                            <div className='d-felx'>
+                        <motion.div className="textCont d-flex flex-column justify-content-center" variants={textVarient} initial="initial"
+                            animate="animate">
+                            <motion.h2 variants={textVarient} initial="initial"
+                                animate="animate">NEEHARIKA S</motion.h2>
+                            <motion.h1 variants={textVarient} initial="initial"
+                                animate="animate">Cyber Security Analyst</motion.h1>
+                            <motion.div variants={textVarient} className='d-felx' initial="initial"
+                                animate="animate">
                                 <Button variant='outline-light' className='p-3 me-4'>See the Latest Works</Button>
                                 <Button variant='light p-3'>Contact Me</Button>
-                            </div>
-                        </div>
-                        <motion.div className='slideText' 
-                                    variant={sliderVariant} 
-                                    initial="initial" 
-                                    animate="animate">
-                            Mernstack Developer, Pen Tester, Flutter Developer
+                            </motion.div>
+                        </motion.div>
+                        <motion.div className='slideText d-flex'
+                            variants={sliderVariant}
+                            initial="initial"
+                            animate="animate">
+                            Web Dev, Penetration Tester, Flutter Developer
                         </motion.div>
                     </Col>
                     <Col lg={6}>
                         <div className='imgCont d-flex justify-content-end'>
-                            <img className='img-fluid' src={heroImg} alt="" />
+                            <motion.img className='img-fluid' src={heroImg} alt="" variants={imgVarient} initial="initial"
+                                animate="animate" />
                         </div>
                     </Col>
                 </Row>
             </Container>
-
         </div>
     )
 }
